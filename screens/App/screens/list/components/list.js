0dbac3e1e2da5c712d19b070095b549eb5f1b179
Sourabh UI <sourabhgupta.rp@gmail.com>
2015-12-01 18:11:49 +0530

    updated class names

--- screens/App/screens/list/components/list.js
+++ screens/App/screens/list/components/list.js
@@ -3,8 +3,8 @@ import React from 'react';
 class AddList extends React.Component {
     render(){
         return (
-            <div className="components-container">
-                <ul className="components-heading">
+            <div className="components-list-container">
+                <ul className="components-list--heading">
                     <li>
                         <div>Title</div>
                         <div>Description</div>
