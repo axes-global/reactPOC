75fd857276df114a4e0980f4d69da6ca7e2c82d5
Sourabh UI <sourabhgupta.rp@gmail.com>
2015-12-01 18:59:40 +0530

    updated

--- screens/App/screens/list/components/list.js
+++ screens/App/screens/list/components/list.js
@@ -4,7 +4,7 @@ class AddList extends React.Component {
     render(){
         return (
             <div className="components-list-container">
-                <ul className="components-list--heading">
+                <ul className="components-list-heading">
                     <li>
                         <div>Title</div>
                         <div>Description</div>
