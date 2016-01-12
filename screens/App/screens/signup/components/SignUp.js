ae746c69fdaf5ea1e3aebfa8b4587f4e306b1f3d
rahulv <rahulv@techaspect.com>
2016-01-12 15:36:03 +0530

    markup for the signup screen

--- screens/App/screens/signup/components/SignUp.js
+++ screens/App/screens/signup/components/SignUp.js
@@ -3,7 +3,33 @@ import React from 'react';
 class SignUp extends React.Component {
     render() {
         return (
-            <h1>SignUp Component-new</h1>
+        	<div className="signup-container">
+            <h1>Create Your Account</h1>
+            <form className="signup-form">
+            	<div className="credentials-wrapper">
+	            	<label>Username</label>
+	                <span>
+					  <i className="fa fa-user"></i>
+					  <input type="text" name="Username" placeholder="Username"/>
+					</span>
+	            </div>    
+	            <div className="credentials-wrapper">
+	            	<label>E-mail Address</label>
+	                <span>
+ 						<i className="fa fa-envelope"></i>
+  						<input type="email" data-reactid=".0.1.1.1.1" placeholder="E-mail Address" name="email"/>
+  					</span>
+	            </div>
+	            <div className="credentials-wrapper">
+	            	<label>Password</label>
+	                <span>
+						<i className="fa fa-key"></i>
+						<input type="password" name="password" placeholder="Password"/>
+					</span>
+	            </div>
+	            <input type="button" value="Sign Up" className="signup-button"/>
+            </form>
+            </div>
         )
     }
 }
