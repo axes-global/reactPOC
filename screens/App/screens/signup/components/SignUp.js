4b876e9984e8e85077b670f96487cb866615449e
rahulv <rahulv@techaspect.com>
2016-01-13 16:05:15 +0530

    added markup for asterisk

--- screens/App/screens/signup/components/SignUp.js
+++ screens/App/screens/signup/components/SignUp.js
@@ -7,21 +7,21 @@ class SignUp extends React.Component {
             <h1>Create Your Account</h1>
             <form className="signup-form">
             	<div className="credentials-wrapper">
-	            	<label>Username</label>
+	            	<label>Username<span className="required">*</span></label>
 	                <span>
 					  <i className="fa fa-user"></i>
 					  <input type="text" name="Username" placeholder="Username" required/>
 					</span>
 	            </div>    
 	            <div className="credentials-wrapper">
-	            	<label>E-mail Address</label>
+	            	<label>E-mail Address<span className="required">*</span></label>
 	                <span>
  						<i className="fa fa-envelope"></i>
   						<input type="email" placeholder="E-mail Address" name="email" required/>
   					</span>
 	            </div>
 	            <div className="credentials-wrapper">
-	            	<label>Password</label>
+	            	<label>Password<span className="required">*</span></label>
 	                <span>
 						<i className="fa fa-key"></i>
 						<input type="password" name="password" placeholder="Password" required/>
