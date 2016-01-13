27338dcabda67e1318a12e724c46f929d2be0024
rahulv <rahulv@techaspect.com>
2016-01-13 13:25:31 +0530

    added footer.js

---
+++ screens/App/screens/shared/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyFooter extends React.Component {
+    render() {
+        return (
+            <div className="footer-container">
+                <span>&copy; 2016 - <a href="http://www.techaspect.com/" target="_blank" title="Visit site">www.techaspect.com</a> - center of excellece</span>
+            </div>
+        );
+    }
+}
+
+export default MyFooter;
