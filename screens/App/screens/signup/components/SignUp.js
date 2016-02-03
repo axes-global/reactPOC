45654aafbb3d4d5562946e58da57fc811a83839d
rahulv <rahulv@techaspect.com>
2016-02-03 16:27:00 +0530

    included code for exporting signupcontroller

--- screens/App/screens/signup/components/SignUp.js
+++ screens/App/screens/signup/components/SignUp.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { store } from '../../controller/signUpController';
 class SignUp extends React.Component {
     render() {
         return (
         	<div className="signup-container">
             <h1>Create Your Account</h1>
-            <form className="signup-form">
+            <form className="signup-form" onSubmit = {store} >
             	<div className="credentials-wrapper">
 	            	<label>Username<span className="required">*</span></label>
 	                <span>
@@ -26,9 +26,9 @@ class SignUp extends React.Component {
 						<i className="fa fa-key"></i>
 						<input type="password" name="password" placeholder="Password" required/>
 					</span>
-	            </div>
+	            </div> 
 	            <div className="msg-subscribe">You have been successfully subscribed !!</div>
-	            <input type="submit" value="Sign Up" className="signup-button"/>
+	            <input type="submit" value="Sign Up" className="signup-button" />
             </form>
             </div>
         )
