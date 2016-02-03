7fdccbd8f05dfb14738f3b17e4cc5ce3a3850254
rahulv <rahulv@techaspect.com>
2016-02-03 16:26:04 +0530

    added content for listing

--- screens/App/screens/list/components/list.js
+++ screens/App/screens/list/components/list.js
@@ -13,28 +13,52 @@ class AddList extends React.Component {
                         <div>Uploaded Date</div>
                     </li>
                     <li>
-                        <div>Title</div>
-                        <div>Description</div>
-                        <div>Amount</div>
-                        <div>Uploaded Date</div>
+                        <div>Mobile to be sold</div>
+                        <div>
+                            <ul className="component-desc-list">
+                                <li>Iphone 5s</li>
+                                <li>1 year old</li>
+                                <li>with waranty card and earphones</li>
+                            </ul>     
+                        </div>
+                        <div>20000</div>
+                        <div>25-08-2015</div>
                     </li>
                     <li>
-                        <div>Title</div>
-                        <div>Description</div>
-                        <div>Amount</div>
-                        <div>Uploaded Date</div>
+                        <div>Room for sharing</div>
+                        <div>
+                            <ul className="component-desc-list">
+                                <li>2 Rooms available</li>
+                                <li>Near to office</li>
+                                <li>With Kitchen and food facility</li>
+                            </ul>
+                        </div>
+                        <div>5000 per person</div>
+                        <div>10-9-2015</div>
                     </li>
                     <li>
-                        <div>Title</div>
-                        <div>Description</div>
-                        <div>Amount</div>
-                        <div>Uploaded Date</div>
+                        <div>Bike to be sold</div>
+                        <div>
+                            <ul className="component-desc-list">
+                                <li>Passion Pro 2010 model</li>
+                                <li>35000kms run</li>
+                                <li>5 years old</li>
+                            </ul>
+                        </div>
+                        <div>35000</div>
+                        <div>15-10-2015</div>
                     </li>
                     <li>
-                        <div>Title</div>
-                        <div>Description</div>
-                        <div>Amount</div>
-                        <div>Uploaded Date</div>
+                        <div>Car Pooling</div>
+                        <div>
+                            <ul className="component-desc-list">
+                                <li>Have i20</li>
+                                <li>3 seats available</li>
+                                <li>From Subash Nagar</li>
+                            </ul>
+                        </div>
+                        <div>500 per person</div>
+                        <div>20-12-2015</div>
                     </li>
                 </ul>
             </div>	
